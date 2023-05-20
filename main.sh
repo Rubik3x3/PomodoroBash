b84dde9bb8bfe4e494fc673b6fd36591f5c73d3b
Rubik3x3 <franco.f4629@gmail.com>
2023-05-19 21:39:13 -0300

    Funcion programar pomodoro

--- main.sh
+++ main.sh
@@ -51,7 +51,7 @@ do
         titulo_inicio
 
         echo "[ 1 ] Programar Pomodoro"
-        echo "[ 2 ] Programar Alarma"
+        echo "[ 2 ] Programar Alarma (NO FUNCIONA AÚN)"
         echo -e "[ 3 ] Salir\n"
         echo -n "Opción: "
         read -n1 opc
@@ -61,14 +61,12 @@ do
         case $opc in
         1)
             p=1
-            echo "Has ingresado el número 1."
             ;;
         2)
-            p=2
-            echo "Has ingresado el número 2."
+            p=0
             ;;
         3)
-            echo "Has ingresado el número 3."
+            pkill -f main.sh
             ;;
         *)
             echo "Has ingresado un número invalido."
@@ -77,30 +75,55 @@ do
         ;;
     1)
         titulo_programar
-        echo -n "1/2) Ingrese la duración en minutos del Pomodoro: "
+        echo -n "1/2) Ingrese la duración en minutos del Pomodoro (max 60m): "
         read -n2 pomodoro
         echo -e "\n"
-        echo -n "2/2) Ingrese el descanzo en minutos: "
+        echo -n "2/2) Ingrese el descanzo en minutos (max 60m): "
         read -n2 descanzo
-
         echo -e "\n"
-
-        
-        titulo_programar
-
         echo -e "Pomodoro programado correctamente.\n"
+        notify-send 'POMODORO' 'Programado correctamente.'
         echo -e "Pomodoro: $pomodoro minutos | Descanzo: $descanzo minutos"
 
-        segActuales=0
-        segTotales=$pomodoro*60        
-        echo $segTotales
-        echo "Segundos $segActuales"
-        while [ $segActuales -lt $segTotales ]
+        let segTotales=$pomodoro*60
+        let descTotales=$descanzo*60
+
+        echo -en "\r>> Minutos restantes: $pomodoro"
+        while [ $segTotales -gt 0 ]
         do
             sleep 1
-            $segActuales=$segActuales+1
-            echo -en "\rSegundos $segActuales"
+            ((segTotales--))
+            let minutos=$segTotales/60
+            echo -en "\r>> Minutos restantes: $minutos "
         done
+        notify-send 'POMODORO' 'Temporizador terminado, ¡Puedes descanzar!'
+        echo -e -n "\n¿Quieres empezar la pausa? [s/n]"
+        read -n1 ans
+        if [[ $ans = 's' ]]
+        then
+        echo -en "\r                                                          "
+        echo -en "\r>> Minutos restantes de pausa: $descanzo"
+        while [ $descTotales -gt 0 ]
+        do
+            sleep 1
+            ((descTotales--))
+            let minutosP=$descTotales/60
+            echo -en "\r>> Minutos restantes de pausa: $minutosP "
+        done
+        else
+            notify-send 'POMODORO' "Saliste del pomodoro."
+            p=0
+        fi
+
+        echo -e "\nPOMODORO FINALIZADO!\n"
+        notify-send 'POMODORO' 'POMODORO FINALIZADO!'
+        echo -n "¿Quiere continuar con otro pomodoro? [s/n]: "
+        read -n1 ansF
+
+        if [[ $ansF = 'n' ]]
+        then
+        p=0
+        fi
         ;;
     2)
         titulo_alarma
